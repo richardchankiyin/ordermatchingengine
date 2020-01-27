cefeb61c52f4633857618c6c02367435b875dc3d
richardchankiyin <richardchankiyin@gmail.com>
2020-01-27 22:37:54 +0800

    update SpreadRangesTest

--- ordermatchingengine/src/test/java/com/richardchankiyin/spreadcalc/SpreadRangesTest.java
+++ ordermatchingengine/src/test/java/com/richardchankiyin/spreadcalc/SpreadRangesTest.java
@@ -21,4 +21,34 @@ public class SpreadRangesTest {
 		assertFalse(SpreadRanges.getInstance().isValidPrice(0.2495, true));
 	}
 	
+	@Test
+	public void testIsValidSecondRangeStartingGoUp() {
+		assertTrue(SpreadRanges.getInstance().isValidPrice(0.25, true));
+	}
+	
+	@Test
+	public void testIsValidSecondRangeMidValuesGoUp() {
+		assertTrue(SpreadRanges.getInstance().isValidPrice(0.255, true));
+		assertTrue(SpreadRanges.getInstance().isValidPrice(0.260, true));
+		assertTrue(SpreadRanges.getInstance().isValidPrice(0.490, true));
+		assertTrue(SpreadRanges.getInstance().isValidPrice(0.495, true));
+		assertFalse(SpreadRanges.getInstance().isValidPrice(0.2555, true));
+		assertFalse(SpreadRanges.getInstance().isValidPrice(0.4955, true));
+	}
+	
+	@Test
+	public void testIsValidThirdRangeStartingGoUp() {
+		assertTrue(SpreadRanges.getInstance().isValidPrice(0.50, true));
+	}
+	
+	@Test
+	public void testIsValidThirdRangeMidValuesGoUp() {
+		assertTrue(SpreadRanges.getInstance().isValidPrice(0.51, true));
+		assertTrue(SpreadRanges.getInstance().isValidPrice(0.52, true));
+		assertTrue(SpreadRanges.getInstance().isValidPrice(9.98, true));
+		assertTrue(SpreadRanges.getInstance().isValidPrice(9.99, true));
+		assertFalse(SpreadRanges.getInstance().isValidPrice(0.511, true));
+		assertFalse(SpreadRanges.getInstance().isValidPrice(9.995, true));
+	}
+	
 }
