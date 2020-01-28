416c09587c287fdfc8ec3b217b42cc61a85e382c
richardchankiyin <richardchankiyin@gmail.com>
2020-01-28 18:27:19 +0800

    Add IncomingOrderValidatorTest

--- ordermatchingengine/src/test/java/com/richardchankiyin/ordermatchingengine/order/validation/IncomingOrderValidatorTest.java
+++ ordermatchingengine/src/test/java/com/richardchankiyin/ordermatchingengine/order/validation/IncomingOrderValidatorTest.java
@@ -36,5 +36,42 @@ public class IncomingOrderValidatorTest {
 		
 		assertTrue(r.validate(oe).isAccepted());
 	}
+	
+	@Test
+	public void testDataTypeCheckingRuleTag38NotValidTag44Valid() {
+		OrderValidationRule r = IncomingOrderValidator.getInstance().getDataTypeCheckingRule();
+		OrderEvent oe = new OrderEvent();
+		oe.put(38, "111a");
+		oe.put(44, 111.5);
+		
+		OrderValidationResult result = r.validate(oe);
+		assertFalse(result.isAccepted());
+		assertEquals("DATATYPECHECKING->Tag 38: 111a is not integer. ", result.getRejectReason());
+	}
+	
+	@Test
+	public void testDataTypeCheckingRuleTag38ValidTag44NotValid() {
+		OrderValidationRule r = IncomingOrderValidator.getInstance().getDataTypeCheckingRule();
+		OrderEvent oe = new OrderEvent();
+		oe.put(38, 111);
+		oe.put(44, "111.5a");
+		
+		OrderValidationResult result = r.validate(oe);
+		assertFalse(result.isAccepted());
+		assertEquals("DATATYPECHECKING->Tag 44: 111.5a is not numeric. ", result.getRejectReason());
+		
+	}
+	
+	@Test
+	public void testDataTypeCheckingRuleTag38NotValidTag44NotValid() {
+		OrderValidationRule r = IncomingOrderValidator.getInstance().getDataTypeCheckingRule();
+		OrderEvent oe = new OrderEvent();
+		oe.put(38, "111a");
+		oe.put(44, "111.5a");
+		
+		OrderValidationResult result = r.validate(oe);
+		assertFalse(result.isAccepted());
+		assertEquals("DATATYPECHECKING->Tag 38: 111a is not integer. Tag 44: 111.5a is not numeric. ", result.getRejectReason());
+	}
 
 }
