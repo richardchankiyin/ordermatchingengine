a80c3e7b69767b37e875befce228748acd55c36d
richardchankiyin <richardchankiyin@gmail.com>
2020-01-28 20:12:26 +0800

    Add IncomingOrderValidatorTest

--- ordermatchingengine/src/test/java/com/richardchankiyin/ordermatchingengine/order/validation/IncomingOrderValidatorTest.java
+++ ordermatchingengine/src/test/java/com/richardchankiyin/ordermatchingengine/order/validation/IncomingOrderValidatorTest.java
@@ -137,4 +137,48 @@ public class IncomingOrderValidatorTest {
 		assertEquals("NEWORDERSINGLECOMPULSORYFIELDCHECKING->Tag 11: ClOrdId, Tag 38: OrderQty, Tag 44: Price, Tag 54: Side and Tag 55: Symbol cannot be missing for limit order. ", result.getRejectReason());
 	}
 	
+	@Test
+	public void testNewOrderSingleCompulsoryFieldCheckingRuleMarketOrderMissingOrderQty() {
+		OrderValidationRule r = IncomingOrderValidator.getInstance().getNewOrderSingleCompulsoryFieldChecking();
+		OrderEvent oe = new OrderEvent();
+		oe.put(11, "1111");
+		oe.put(35, "D");
+		oe.put(40, "1");
+		oe.put(54, "1");
+		oe.put(55, "0005.HK");
+		OrderValidationResult result = r.validate(oe);
+		assertFalse(result.isAccepted());
+		assertEquals("NEWORDERSINGLECOMPULSORYFIELDCHECKING->Tag 11: ClOrdId, Tag 38: OrderQty, Tag 54: Side and Tag 55: Symbol cannot be missing for market order. ", result.getRejectReason());
+	}
+	
+	@Test
+	public void testNewOrderSingleCompulsoryFieldCheckingRuleLimitOrderMissingOrderQty() {
+		OrderValidationRule r = IncomingOrderValidator.getInstance().getNewOrderSingleCompulsoryFieldChecking();
+		OrderEvent oe = new OrderEvent();
+		oe.put(11, "1111");
+		oe.put(35, "D");
+		oe.put(40, "2");
+		oe.put(44, 100);
+		oe.put(54, "1");
+		oe.put(55, "0005.HK");
+		OrderValidationResult result = r.validate(oe);
+		assertFalse(result.isAccepted());
+		assertEquals("NEWORDERSINGLECOMPULSORYFIELDCHECKING->Tag 11: ClOrdId, Tag 38: OrderQty, Tag 44: Price, Tag 54: Side and Tag 55: Symbol cannot be missing for limit order. ", result.getRejectReason());
+	}
+	
+	@Test
+	public void testNewOrderSingleCompulsoryFieldCheckingRuleLimitOrderMissingPrice() {
+		OrderValidationRule r = IncomingOrderValidator.getInstance().getNewOrderSingleCompulsoryFieldChecking();
+		OrderEvent oe = new OrderEvent();
+		oe.put(11, "11111");
+		oe.put(35, "D");
+		oe.put(38, 100);
+		oe.put(40, "2");
+		oe.put(54, "1");
+		oe.put(55, "0005.HK");
+		OrderValidationResult result = r.validate(oe);
+		assertFalse(result.isAccepted());
+		assertEquals("NEWORDERSINGLECOMPULSORYFIELDCHECKING->Tag 11: ClOrdId, Tag 38: OrderQty, Tag 44: Price, Tag 54: Side and Tag 55: Symbol cannot be missing for limit order. ", result.getRejectReason());
+	}
+	
 }
