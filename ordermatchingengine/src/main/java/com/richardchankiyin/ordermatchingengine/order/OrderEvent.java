89f8d41f3e9aa85d055bcb526eb45f7ab57b0ca9
richardchankiyin <richardchankiyin@gmail.com>
2020-01-28 10:02:45 +0800

    add OrderEvent

---
+++ ordermatchingengine/src/main/java/com/richardchankiyin/ordermatchingengine/order/OrderEvent.java
@@ -0,0 +1,12 @@
+package com.richardchankiyin.ordermatchingengine.order;
+
+import java.util.HashMap;
+
+public class OrderEvent extends HashMap<Integer, Object> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
