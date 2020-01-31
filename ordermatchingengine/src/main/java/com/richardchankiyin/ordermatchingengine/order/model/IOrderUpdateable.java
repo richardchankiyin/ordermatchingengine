2e42ae28ecc5219dc8e36d82c3e885991f588821
richardchankiyin <richardchankiyin@gmail.com>
2020-02-01 01:10:05 +0800

    add IOrderUpdatable

---
+++ ordermatchingengine/src/main/java/com/richardchankiyin/ordermatchingengine/order/model/IOrderUpdateable.java
@@ -0,0 +1,8 @@
+package com.richardchankiyin.ordermatchingengine.order.model;
+
+import com.richardchankiyin.ordermatchingengine.order.OrderEvent;
+
+public interface IOrderUpdateable {
+	public void updateOrder(OrderEvent oe);
+
+}
