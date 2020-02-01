7d9a6dbe55c5867dc12b55cc3955425a14977ac3
richardchankiyin <richardchankiyin@gmail.com>
2020-02-01 14:35:59 +0800

    add OrderStateMachine test

--- ordermatchingengine/src/test/java/com/richardchankiyin/ordermatchingengine/order/statemachine/OrderStateMachineTest.java
+++ ordermatchingengine/src/test/java/com/richardchankiyin/ordermatchingengine/order/statemachine/OrderStateMachineTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import com.richardchankiyin.ordermatchingengine.order.OrderEvent;
 import com.richardchankiyin.ordermatchingengine.order.model.IOrderModel;
 import com.richardchankiyin.ordermatchingengine.order.model.IOrderUpdateable;
+import com.richardchankiyin.ordermatchingengine.order.validation.OrderValidationResult;
 
 public class OrderStateMachineTest {
 
@@ -33,8 +34,39 @@ public class OrderStateMachineTest {
 		oe.put(35, "D");
 		oe.put(39, "A");
 		assertTrue(om.getNosFromNonexistingToPendingNew().validate(oe).isAccepted());
+	}
+	
+	@Test
+	public void testNosFromNonexistingToPendingNewValidationRuleWithClOrdIdFound() {
+		IOrderModel model = new IOrderModel() {
+			public boolean isClientOrderIdFound(String clientOrderId) {
+				return "1111".equals(clientOrderId);
+			}
+			
+			public OrderEvent getOrder(String clientOrderId) {
+				OrderEvent oe = new OrderEvent();
+				oe.put(11, "1111");
+				oe.put(39, "A");
+				return oe;
+			}
+		};
+		
+		IOrderUpdateable orderUpdateable = new IOrderUpdateable() {
+			@Override
+			public void updateOrder(OrderEvent oe) {}
+		};
+		
+		OrderStateMachine om = new OrderStateMachine(model, orderUpdateable);
+		OrderEvent oe = new OrderEvent();
+		oe.put(11, "1111");
+		oe.put(35, "D");
+		oe.put(39, "A");
+		OrderValidationResult result = om.getNosFromNonexistingToPendingNew().validate(oe);
+		assertFalse(result.isAccepted());
+		assertEquals("NOSFROMNONEXISTINGTOPENDINGNEW->Tag 11: ClOrdId : 1111 exists, cannot create an order with this value. ", result.getRejectReason());
 		
 	}
+	
 	
 	@Test
 	public void testNosFromNonexistingToPendingNewValidationRuleRejected() {
@@ -53,15 +85,52 @@ public class OrderStateMachineTest {
 			public void updateOrder(OrderEvent oe) {}
 		};
 		
-		// Non-exist -> New Rejected
+		// Non-exist -> New: Rejected
 		OrderStateMachine om = new OrderStateMachine(model, orderUpdateable);
 		OrderEvent oe = new OrderEvent();
 		oe.put(11, "1111");
 		oe.put(35, "D");
 		oe.put(39, "0");
-		assertFalse(om.getNosFromNonexistingToPendingNew().validate(oe).isAccepted());
+		OrderValidationResult result = om.getNosFromNonexistingToPendingNew().validate(oe);
+		assertFalse(result.isAccepted());
+		assertEquals("NOSFROMNONEXISTINGTOPENDINGNEW->Tag 39 must be A for a blank new order creation. ", result.getRejectReason());
 		
+		// Non-exist -> Suspended: Rejected
+		oe = new OrderEvent();
+		oe.put(11, "1111");
+		oe.put(35, "D");
+		oe.put(39, "9");
+		result = om.getNosFromNonexistingToPendingNew().validate(oe);
+		assertFalse(result.isAccepted());
+		assertEquals("NOSFROMNONEXISTINGTOPENDINGNEW->Tag 39 must be A for a blank new order creation. ", result.getRejectReason());
 		
+		// Non-exist -> PartiallyFilled: Rejected
+		oe = new OrderEvent();
+		oe.put(11, "1111");
+		oe.put(35, "D");
+		oe.put(39, "1");
+		result = om.getNosFromNonexistingToPendingNew().validate(oe);
+		assertFalse(result.isAccepted());
+		assertEquals("NOSFROMNONEXISTINGTOPENDINGNEW->Tag 39 must be A for a blank new order creation. ", result.getRejectReason());
+		
+		// Non-exist -> Filled: Rejected
+		oe = new OrderEvent();
+		oe.put(11, "1111");
+		oe.put(35, "D");
+		oe.put(39, "2");
+		result = om.getNosFromNonexistingToPendingNew().validate(oe);
+		assertFalse(result.isAccepted());
+		assertEquals("NOSFROMNONEXISTINGTOPENDINGNEW->Tag 39 must be A for a blank new order creation. ", result.getRejectReason());
+		
+		// Non-exist -> DFD: Rejected
+		oe = new OrderEvent();
+		oe.put(11, "1111");
+		oe.put(35, "D");
+		oe.put(39, "3");
+		result = om.getNosFromNonexistingToPendingNew().validate(oe);
+		assertFalse(result.isAccepted());
+		assertEquals("NOSFROMNONEXISTINGTOPENDINGNEW->Tag 39 must be A for a blank new order creation. ", result.getRejectReason());
+
 	}
 
 }
