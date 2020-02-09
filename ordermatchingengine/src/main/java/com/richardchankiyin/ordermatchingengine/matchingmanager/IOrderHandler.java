fddc595bad4d1896d883ec0e143bf54af75d4bf8
richardchankiyin <richardchankiyin@gmail.com>
2020-02-09 22:57:29 +0800

    update PriceOrderQueue test

--- ordermatchingengine/src/main/java/com/richardchankiyin/ordermatchingengine/matchingmanager/IOrderHandler.java
+++ ordermatchingengine/src/main/java/com/richardchankiyin/ordermatchingengine/matchingmanager/IOrderHandler.java
@@ -1,15 +1,11 @@
 package com.richardchankiyin.ordermatchingengine.matchingmanager;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.richardchankiyin.ordermatchingengine.order.OrderEvent;
 import com.richardchankiyin.ordermatchingengine.order.validation.AbstractOrderValidator;
 import com.richardchankiyin.ordermatchingengine.order.validation.OrderValidationResult;
 
 public interface IOrderHandler {
-	static final Logger logger = LoggerFactory.getLogger(IOrderHandler.class);
 	public void addOrder(OrderEvent oe);
 	public void updateOrder(OrderEvent oe);
 	public void cancelOrder(OrderEvent oe);
@@ -18,7 +14,6 @@ public interface IOrderHandler {
 		OrderValidationResult validationResult = validator.validate(oe);
 		if (!validationResult.isAccepted()) {
 			String rejectReason = validationResult.getRejectReason();
-			logger.debug("OrderEvent: {} validator: {} Rejected Reason: {}", oe, validator, rejectReason);
 			throw new IllegalArgumentException(rejectReason);
 		}
 	}
