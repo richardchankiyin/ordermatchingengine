5490c950052f4848598b6aa29fd5cfddb794a768
richardchankiyin <richardchankiyin@gmail.com>
2020-02-23 02:51:44 +0800

    update MatchingManager

--- ordermatchingengine/src/main/java/com/richardchankiyin/ordermatchingengine/matchingmanager/ExecutionBook.java
+++ ordermatchingengine/src/main/java/com/richardchankiyin/ordermatchingengine/matchingmanager/ExecutionBook.java
@@ -1,6 +1,5 @@
 package com.richardchankiyin.ordermatchingengine.matchingmanager;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
