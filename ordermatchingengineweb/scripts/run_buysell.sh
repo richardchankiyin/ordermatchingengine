8c9522088ac773b1b7605a51ec7b98819cdb0154
richardchankiyin <richardchankiyin@gmail.com>
2020-03-01 13:29:50 +0800

    add scripts to load order

---
+++ ordermatchingengineweb/scripts/run_buysell.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+host=localhost
+port=8080
+
+for x in $(grep -v "^#" buysell.txt)
+do s="$(echo $x | cut -d',' -f1)"; p="$(echo $x | cut -d',' -f2)"; q="$(echo $x | cut -d',' -f3)"; 
+content=$(echo "{\"side\":\"$s\",\"symbol\":\"0005.HK\",\"ordType\":\"2\",\"price\":$p,\"quantity\":$q}"); 
+
+echo $content
+curl -s http://${host}:${port}/order -H "Content-type:application/json" -d "$content"
+done
