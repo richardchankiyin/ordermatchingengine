5abff9fa973b2d0312f4fe35b510283ff006ce35
richardchankiyin <richardchankiyin@gmail.com>
2020-03-01 14:46:15 +0800

    add market scripts

---
+++ ordermatchingengineweb/scripts/run_buysell_market.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+host=localhost
+port=8080
+
+for x in $(grep -v "^#" buysell_market.txt)
+do s="$(echo $x | cut -d',' -f1)"; q="$(echo $x | cut -d',' -f2)";  
+content=$(echo "{\"side\":\"$s\",\"symbol\":\"0005.HK\",\"ordType\":\"1\",\"quantity\":$q}"); 
+
+echo $content
+curl -s http://${host}:${port}/order -H "Content-type:application/json" -d "$content"
+done
